3239a103dc87182a1d4f20fa68a658ec2ec2ec44
Quinn Casey <qcasey1@gmail.com>
2019-09-18 12:20:21 -0700

    adding setup for pip

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='MDroid-PythonConfig',  
+     version='0.1',
+     scripts=['config'] ,
+     author="Quinn Casey",
+     author_email="quinn@quinncasey.com",
+     description="Reads from MDroid configuration file, used across several Python scripts in the MDroid suite.",
+     url="https://github.com/MrDoctorKovacic/MDroid-PythonConfig",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )

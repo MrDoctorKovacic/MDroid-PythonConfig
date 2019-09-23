752c44688d615a37296e0caaf4ec304e2e97b6fd
Quinn Casey <qcasey1@gmail.com>
2019-09-23 10:50:19 -0700

    updating config

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='MDroidPythonConfig',  
+     name='mdroidconfig',  
      version='0.1',
-     scripts=['mdroidconfig'] ,
      author="Quinn Casey",
      author_email="quinn@quinncasey.com",
      description="Reads from MDroid configuration file, used across several Python scripts in the MDroid suite.",
      url="https://github.com/MrDoctorKovacic/MDroid-PythonConfig",
-     packages=setuptools.find_packages(),
+     packages=['mdroidconfig'],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
